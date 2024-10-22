2077dcf265a603978b7c66faa761664fd07b1f01
eyahushiepane <eyahushiepane@gmail.com>
2024-10-22 15:38:40 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'BSIT4-DINAMPO',
+  webDir: 'dist'
+};
+
+export default config;
